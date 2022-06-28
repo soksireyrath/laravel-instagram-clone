8d45f4130ed0b4c72d67bf5d00f34a627e8f2d6b
sireyrath <soksireyrath18@kit.edu.kh>
2022-06-28 11:13:59 +0700

    completed profile frontend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,53 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="row">
+        <div class="col-2">
+            <img src="/image/profile.svg" style="width:140px;" class="rounded-circle" alt="profile image" />
+        </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+        <div class="col-9 ">
+            <div>Anne_Marie</div>
+            <div class="d-flex pt-2">
+                <div class="mt-3"><strong>0</strong>posts</div>
+                <div class="m-3"><strong>245</strong>followers</div>
+                <div class="m-3"><strong>147</strong>following</div>
             </div>
+            <div class="pt-1">I'm a vocalist and Cute ><</div>
+        </div>
+        
+        <div class="row  pt-5">
+            <div class="col-4">
+                <img src="/image/post1.jpg" style='width:400px'/> 
+            </div>
+            <div class="col-4">
+                <img src="/image/post2.jpg" style='width:400px'/> 
+            </div>
+            <div class="col-4 ">
+                <img src="/image/post3.jpg" style='width:400px'/> 
+            </div>
+
+            <div class="col-4 ">
+                <img src="/image/post4.jpg" style='width:400px'/> 
+            </div>
+            <div class="col-4 ">
+                <img src="/image/post5.jpg" style='width:400px'/> 
+            </div>
+            <div class="col-4 ">
+                <img src="/image/post6.jpg" style='width:400px'/> 
+            </div>
+            
+        </div>
+
+
+
+
+
+          
         </div>
     </div>
+
+
+    
 </div>
 @endsection
